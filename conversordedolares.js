cf294f80f1fe68d2917ef6e2747c7dc9e15b92a5
malejalara <malejalara01@gmail.com>
2021-08-29 07:20:28 -0500

    conversor

--- conversordedolares.js
+++ conversordedolares.js
@@ -5,11 +5,12 @@ function pesoColombiano() {
     resultado= montoenpesos / dolarActual 
     tasadecambio=resultado * 0.07
     valorfinal=resultado-tasadecambio
+  
 
     if(resultado<=0){
         alert('operacion no valida')
     }
     else{
-        alert('el monto en dolares es '+ Math.round(resultado)+ 'restando la tasa de cambio el valor final es de '+ Math.round(valorfinal))
+        alert('El monto en dolares es '+ resultado.toFixed(2) + '\nRestando la tasa de cambio el valor final es de' + valorfinal.toFixed(2));
     }   
-}
+} 
